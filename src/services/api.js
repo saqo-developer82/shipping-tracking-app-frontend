3137bc0fd728066e9df6eb4a343cf05d973d02fa
Sargis <sargis.torosyan@esterox.am>
2025-05-30 12:40:08 +0400

    added env file and removed not used files

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
 
 class ApiService {
     async trackPackage(trackingCode) {
